cd306713d0eb191462d8c478c9663aba129c8dcb
Matheus Pessoa <matheus41.dev@gmail.com>
2026-09-13 20:14:46 -0300

    Add project files.

---
+++ ProductClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure
+{
+    public class ProductClientHubDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=D:\\Workspace\\1737062251373-attachment.octet-stream");
+        }
+    }
+}
---
+++ ProductClientHub.API/Program.cs
@@ -0,0 +1,31 @@
+using ProductClientHub.API.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddMvc(option => option.Filters.Add(typeof(ExceptionFilter)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "ProductClientHub.API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
